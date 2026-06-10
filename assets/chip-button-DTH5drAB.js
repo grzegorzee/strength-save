66d28bb09956964bf4325b4be49429992b0c0f83
Grzegorz Jasionowicz <grzegorzee@gmail.com>
2026-06-10 15:14:07 +0200

    Updates

---
+++ assets/chip-button-DTH5drAB.js
@@ -0,0 +1,1 @@
+import{r as e,j as i}from"./react-vendor-ClavYo8A.js";import{b as m}from"./badge-BdN5D1-S.js";import{k as n}from"./index-CQa3Mky3.js";const u=e.forwardRef(({className:r,variant:t,pressed:o,type:s="button",...a},p)=>i.jsx("button",{ref:p,type:s,"aria-pressed":o,className:n(m({variant:t}),"cursor-pointer",r),...a}));u.displayName="ChipButton";export{u as C};
