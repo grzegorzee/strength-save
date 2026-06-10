af1b39acba3e33af879aeddc5340803271a3a9cc
Grzegorz Jasionowicz <grzegorzee@gmail.com>
2026-06-10 16:07:43 +0200

    Updates

--- assets/chip-button-DWom0nkL.js
+++
@@ -1,1 +0,0 @@
-import{r as e,j as i}from"./react-vendor-ClavYo8A.js";import{b as m}from"./badge-CKCJvtjQ.js";import{k as n}from"./index-CuEljMR0.js";const u=e.forwardRef(({className:r,variant:t,pressed:o,type:s="button",...a},p)=>i.jsx("button",{ref:p,type:s,"aria-pressed":o,className:n(m({variant:t}),"cursor-pointer",r),...a}));u.displayName="ChipButton";export{u as C};
