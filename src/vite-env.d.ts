0f538b0d014228cfca2226d3faf2580da5c8c8b1
Grzegorz Jasionowicz <grzegorzee@gmail.com>
2026-07-03 13:43:53 +0200

    chore(ci): usunięcie martwych VITE_ALLOWED_EMAIL/EMAILS z workflow i typów (Z45)

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -10,7 +10,6 @@ interface ImportMetaEnv {
   readonly VITE_FIREBASE_STORAGE_BUCKET: string;
   readonly VITE_FIREBASE_MESSAGING_SENDER_ID: string;
   readonly VITE_FIREBASE_APP_ID: string;
-  readonly VITE_ALLOWED_EMAIL: string;
   readonly VITE_FEATURE_WORKOUT_TIMERS?: string;
 }
 
