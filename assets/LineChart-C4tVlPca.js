30da4075fc2fde41690625a14b6c853e45bcf5ca
Grzegorz Jasionowicz <grzegorzee@gmail.com>
2026-07-20 01:54:22 +0200

    Updates

---
+++ assets/LineChart-C4tVlPca.js
@@ -0,0 +1,1 @@
+import{r as a,X as i,Y as s,s as r}from"./generateCategoricalChart-DIr4atNk.js";import{L as x}from"./chart-config-CX8P7ifP.js";var t=a({chartName:"LineChart",GraphicalChild:x,axisComponents:[{axisType:"xAxis",AxisComp:i},{axisType:"yAxis",AxisComp:s}],formatAxisMap:r});export{t as L};
