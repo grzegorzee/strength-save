fbc04dadd3260a4643e8ace776027bf1ef7aeca5
Grzegorz Jasionowicz <grzegorzee@gmail.com>
2026-08-06 10:42:55 +0200

    Updates

---
+++ assets/AreaChart-BVaz_Feb.js
@@ -0,0 +1,1 @@
+import{r as a,X as r,Y as s,s as i}from"./generateCategoricalChart-Bt2QMLYO.js";import{A as x}from"./Analytics-C4GFOVbD.js";var o=a({chartName:"AreaChart",GraphicalChild:x,axisComponents:[{axisType:"xAxis",AxisComp:r},{axisType:"yAxis",AxisComp:s}],formatAxisMap:i});export{o as A};
--- assets/LineChart-Cd5EKAh7.js
+++
@@ -1,1 +0,0 @@
-import{r as a,X as i,Y as s,s as r}from"./generateCategoricalChart-Dnqt2KUD.js";import{L as x}from"./chart-config-BK7TJAAF.js";var t=a({chartName:"LineChart",GraphicalChild:x,axisComponents:[{axisType:"xAxis",AxisComp:i},{axisType:"yAxis",AxisComp:s}],formatAxisMap:r});export{t as L};
