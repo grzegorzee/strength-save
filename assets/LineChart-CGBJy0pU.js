a488c255c0629a48210b24553847d1c258dad5b2
Grzegorz Jasionowicz <grzegorzee@gmail.com>
2026-08-12 23:03:59 +0200

    Updates

--- assets/LineChart-CGBJy0pU.js
+++
@@ -1,1 +0,0 @@
-import{r as a,X as i,Y as s,s as r}from"./generateCategoricalChart-DFICvDON.js";import{L as x}from"./chart-config-DfH95ufB.js";var t=a({chartName:"LineChart",GraphicalChild:x,axisComponents:[{axisType:"xAxis",AxisComp:i},{axisType:"yAxis",AxisComp:s}],formatAxisMap:r});export{t as L};
---
+++ assets/useHealthConsent-BNqyqR_v.js
@@ -0,0 +1,1 @@
+import{o as t,aM as s}from"./AuthenticatedApp-C8P2Pcg4.js";const n=()=>{var e;const{profile:r}=t();return((e=s(r))==null?void 0:e.healthGranted)!==!1};export{n as u};
