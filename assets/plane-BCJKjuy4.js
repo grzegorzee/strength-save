0dafc9b09249813c3f6a94b126849c9af2358a23
Grzegorz Jasionowicz <grzegorzee@gmail.com>
2026-08-20 12:28:30 +0200

    Updates

---
+++ assets/plane-BCJKjuy4.js
@@ -0,0 +1,11 @@
+import{c as e}from"./index-080nD_E1.js";/**
+ * @license lucide-react v0.462.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const c=e("BatteryLow",[["rect",{width:"16",height:"10",x:"2",y:"7",rx:"2",ry:"2",key:"1w10f2"}],["line",{x1:"22",x2:"22",y1:"11",y2:"13",key:"4dh1rd"}],["line",{x1:"6",x2:"6",y1:"11",y2:"13",key:"1wd6dw"}]]);/**
+ * @license lucide-react v0.462.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const y=e("Plane",[["path",{d:"M17.8 19.2 16 11l3.5-3.5C21 6 21.5 4 21 3c-1-.5-3 0-4.5 1.5L13 8 4.8 6.2c-.5-.1-.9.1-1.1.5l-.3.5c-.2.5-.1 1 .3 1.3L9 12l-2 3H4l-1 1 3 2 2 3 1-1v-3l3-2 3.5 5.3c.3.4.8.5 1.3.3l.5-.2c.4-.3.6-.7.5-1.2z",key:"1v9wt8"}]]);export{c as B,y as P};
---
+++ assets/useHealthConsent-DiRZ4Ns5.js
@@ -0,0 +1,1 @@
+import{o as t,aL as s}from"./AuthenticatedApp-pPN_qyg-.js";const n=()=>{var e;const{profile:r}=t();return((e=s(r))==null?void 0:e.healthGranted)!==!1};export{n as u};
