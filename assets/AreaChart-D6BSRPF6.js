073ec2d3033a4a7ae7737a83dc4d5e9f227b880d
Grzegorz Jasionowicz <grzegorzee@gmail.com>
2026-08-20 16:00:00 +0200

    Updates

--- assets/AreaChart-D6BSRPF6.js
+++
@@ -1,1 +0,0 @@
-import{m as a,X as r,Y as s,n as i}from"./generateCategoricalChart-DBDpYdCh.js";import{A as x}from"./Analytics-BVIezBYa.js";var o=a({chartName:"AreaChart",GraphicalChild:x,axisComponents:[{axisType:"xAxis",AxisComp:r},{axisType:"yAxis",AxisComp:s}],formatAxisMap:i});export{o as A};
